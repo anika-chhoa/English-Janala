419847c5a4d27f5eb35ecfdd29e9d12cdad0fdbe
anika-chhoa <chhoa.anika07@gmail.com>
2026-03-05 15:08:18 +0600

    modal section dynamic and add loading

--- script/index.js
+++ script/index.js
@@ -12,7 +12,63 @@ const removeActive=()=>{
   })
 }
 
+const manageLoading=(status)=>{
+  if(status===true){
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  }else{
+    document.getElementById("spinner").classList.add("hidden");
+    document.getElementById("word-container").classList.remove("hidden");
+  }
+}
+
+const synonyms=(arr)=>{
+  const synonym=arr.map(syn=>`<button class="btn bg-blue-200 font-normal">${syn}</button>`).join(" ");
+  return synonym;
+}
+
+const modalWords=async (id)=>{
+  
+  const modalUrl=`https://openapi.programming-hero.com/api/word/${id}`;
+  const res= await fetch(modalUrl);
+  const json=await res.json();
+  displayModal(json.data);
+}
+
+const displayModal=(modal)=>{
+  console.log(modal);
+  const myModal=document.getElementById("myModal");
+  myModal.innerHTML="";
+  
+  
+  
+  const div=document.createElement("div");
+  div.innerHTML=`
+  <div class="modal-box w-full">
+  <h3 class="text-4xl font-semibold font-bangla mb-8">${modal.word} (<i class="fa-solid fa-microphone-lines"></i>:${modal.pronunciation})</h3>
+  <p class="text-2xl font-semibold">Meaning</p>
+  <p class="font-bangla text-2xl mb-8">${modal.meaning}</p>
+  <p class="text-2xl font-semibold">Example</p>
+  <p class="text-2xl mb-8">${modal.sentence}</p>
+    <p class="font-bangla text-2xl font-semibold mb-8">সমার্থক শব্দ গুলো</p>
+    <div id="synonym-container">
+    ${synonyms(modal.synonyms)}
+    </div>
+    <div class="modal-action justify-start">
+    <form method="dialog">
+    <!-- if there is a button in form, it will close the modal -->
+    <button class="btn btn-active btn-primary">Complete Learning</button>
+    </form>
+    </div>
+    </div>
+    `
+    myModal.appendChild(div);
+    myModal.showModal();
+   
+}
+
 const lessonWords=(id)=>{
+  manageLoading(true);
     const wordsUrl=`https://openapi.programming-hero.com/api/level/${id}`
     fetch(wordsUrl)
     .then(res=>res.json())
@@ -38,26 +94,28 @@ const displayWords=(words)=>{
         <p class="font-medium text-3xl">নেক্সট Lesson এ যান</p>
       </div>
       `;
+      manageLoading(false);
       return;
     }
 
     words.forEach(word=>{
         const card=document.createElement("div");
         card.innerHTML=`
-        <div class="bg-white p-14 text-center rounded-xl">
+        <div class="bg-white p-14 text-center rounded-xl h-full">
         <h2 class="text-3xl font-bold mb-6">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
         <p class="text-xl font-[500] mb-6">Meaning /Pronounciation</p>
         <div class="font-bangla text-3xl font-semibold mb-10">
           ${word.meaning?word.meaning:"অর্থ পাওয়া যায়নি"}/${word.pronunciation?word.pronunciation:"উচ্চারণ পাওয়া যায়নি"}
         </div>
         <div class="flex justify-between items-center">
-          <button onclick="my_modal_5.showModal()" class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="modalWords(${word.id})" class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
         `
     wordsContainer.appendChild(card);
     })
+    manageLoading(false);
 }
 
 const displayLesson = (lessons) => {
